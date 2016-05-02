3c7c703aa9e6f68a935166dadb275499deb3f1fb
Guillermo García <g.gmenendez@alumnos.upm.es>
2016-05-03 00:16:35 +0200

    eliminados archivos innecesarios

--- src/es/upm/dit/isst/tfg/MailAlumnoServlet.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.tfg;
-
-import javax.servlet.http.HttpServlet;
-
-public class MailAlumnoServlet extends HttpServlet{
-	
-	
-
-}
--- src/es/upm/dit/isst/tfg/MailTutorServlet.java
+++
@@ -1,7 +0,0 @@
-package es.upm.dit.isst.tfg;
-
-import javax.servlet.http.HttpServlet;
-
-public class MailTutorServlet extends HttpServlet{
-
-}
